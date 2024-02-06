c0ba0eb2e8e0e3b7cb619479e16be709e9c45fcd
joshberger5 <bergjo7@furman.edu>
2024-02-05 20:08:42 -0500

    added writeup, pre-existing code

---
+++ main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 16:25:10 2024
+
+@author: Josh Berger
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+def placeholder(site):
+    """ Method to answer question 2 """
+    soup = getSoup(site)
+    refs = getReferences(soup)
+    for ref in refs:
+        print(ref)
+
+    
+def getSoup(site):
+    """ Gets a Beautiful Soup obect for a given website """
+    response = requests.get(site)
+    if response.status_code == 200:
+        text = response.text
+        soup = BeautifulSoup(text, 'html.parser')
+        return soup
+
+def getReferences(soup: BeautifulSoup):
+    """ Get an array of referenced URLs for a BeautifulSoup """
+    srcs = soup.find_all(attrs={'src':True})
+    refs = list()
+    for i in range(0, len(srcs)):
+        # refs.append(getSoup(srcs[i].get('src')))
+        refs.append(srcs[i].get('src'))
+    return refs
+
+def main():
+    placeholder('https://www.youtube.com')
+    #placeholder('https://www.amazon.com')
+    #placeholder('https://www.twitter.com')
+    
+if __name__ == '__main__':
+    main()
+    
