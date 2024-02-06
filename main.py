34d7ba13304dfde46e74baed8648856b2a2f2abe
joshberger5 <bergjo7@furman.edu>
2024-02-05 21:12:19 -0500

    redo initial soup and response setup

--- main.py
+++ main.py
@@ -8,35 +8,57 @@ Created on Mon Jan 29 16:25:10 2024
 import requests
 from bs4 import BeautifulSoup
 
-def placeholder(site):
-    """ Function to answer question 2 """
-    soup = getSoup(site)
-    refs = getReferences(soup)
-    for ref in refs:
-        print(ref)
+def question2(site):
+    """ Function to answer Question 2 """
+    # get the response and BeautifulSoup
+    rs = getResponseAndSoup(site)
 
-    
-def getSoup(site):
-    """ Gets a Beautiful Soup obect for a given website """
+    # check if you got the BeautifulSoup
+    # if not, alert the user that the response was invalid
+    if len(rs) == 1:
+        print("Invalid Response Status Code: " + rs[0])
+    # if so, proceed
+    else:
+        refs = getReferencesResponses(rs[1])
+
+def getResponseAndSoup(site):
+    """ Gets a request and (maybe) a BeautifulSoup for a given website """
+    # make a list to be returned
+    lst = []
+
+    # get the response and set it to be returned
     response = requests.get(site)
-    if response.status_code == 200:
-        text = response.text
-        soup = BeautifulSoup(text, 'html.parser')
-        return soup
+    lst.append(response)
 
-def getReferences(soup: BeautifulSoup):
-    """ Gets an array of referenced URLs for a BeautifulSoup """
+    # if there is a valid response, return the BeautifulSoup and response
+    if response.status_code == 200:
+        # grab the HTML text from the response to pass to the constructor
+        text = response.text
+        # create a BeautifulSoup for the response
+        soup = BeautifulSoup(text, 'html.parser')
+        # add theBeautifulSoup to the return
+        lst.append(soup)
+
+    # if not, return just the response
+    return lst
+
+
+def getReferencesResponses(soup: BeautifulSoup):
+    """ Gets an array of all embedded references for a BeautifulSoup as responses """
+    # get all the src tags from the website
     srcs = soup.find_all(attrs={'src':True})
-    refs = list()
+
+    # gather all the urls from the src tags
+    # create a response for them and add them to a list
+    refs = []
     for i in range(0, len(srcs)):
-        # refs.append(getSoup(srcs[i].get('src')))
-        refs.append(srcs[i].get('src'))
+        refs.append(requests.get(srcs[i].get('src')))
+
+    # return the list
     return refs
 
 def main():
-    placeholder('https://www.youtube.com')
-    #placeholder('https://www.amazon.com')
-    #placeholder('https://www.twitter.com')
+    question2('https://www.youtube.com')
     
 if __name__ == '__main__':
     main()
