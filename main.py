8c0134842a0c899a5ef629231b94e4c5c4ff7159
joshberger5 <bergjo7@furman.edu>
2024-02-06 17:21:40 -0500

    added 2c

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ def question2(site):
         for contentType, count in contentList.items():
             percentage = (count/len(refs)) * 100 if len(refs) > 0 else 0
             print(f"{contentType}: {percentage:}%")
+
+        print(domainPercentages(site, rs[1]))
+
+        
         
 
 def getResponseAndSoup(site):
@@ -72,15 +76,15 @@ def getResponseAndSoup(site):
 def getReferencesResponses(soup: BeautifulSoup):
     """ Gets an array of all embedded references for a BeautifulSoup as responses """
     # get all the src tags from the website
-    srcs = soup.find_all(attrs={'src':True})
+    urls = getSourcesURLs(soup)
 
     # gather all the urls from the src tags
     # create a response for them and add them to a list
     refs = []
-    for i in range(0, len(srcs)):
+    for url in urls:
         try:
         # if the request for the source url is valid, add it to the list
-            r = requests.get(srcs[i].get('src'))
+            r = requests.get(url)
             if r.status_code == 200:
                 refs.append(r)
         except:
@@ -89,6 +93,47 @@ def getReferencesResponses(soup: BeautifulSoup):
 
     # return the list
     return refs
+
+def getSourcesURLs(soup: BeautifulSoup):
+    """ Returns all the embedded references on a BeautifulSoup """
+    srcs = srcs = soup.find_all(attrs={'src':True})
+    urls = []
+    for i in range(0, len(srcs)):
+        urls.append(srcs[i].get('src'))
+    return urls
+
+
+def domainPercentages(site: str, soup: BeautifulSoup):
+    """ Calculates the percentage of references that use each domain for a given soup """
+    # get the URL for the site and its references
+    urls = getSourcesURLs(soup)
+    urls.append(site)
+
+    # make a dictionary to hold the domain names with their respective counts
+    domainCounts = dict()
+
+    # add a domain to the dictionary or increase its count
+    for url in urls:
+        # extract the domain from the URL 
+        firstSlash = url.find('/')
+        secondSlash = url.find('/', firstSlash+1)
+        thirdSlash = url.find('/', secondSlash+1)
+        if thirdSlash == -1: thirdSlash = len(url)
+        domain = url[secondSlash+1:thirdSlash]
+
+        # if a given domain is not already in the dictionary, add it with a count of 1
+        # if it already is, increase the count by 1
+        if domain not in domainCounts:
+            domainCounts[url[secondSlash+1:thirdSlash]] = 1
+        else:
+            domainCounts[url[secondSlash+1:thirdSlash]] += 1
+
+    # convert the counts to percentages
+    for key in domainCounts.keys():
+        domainCounts[key] = domainCounts[key] / len(urls) * 100
+
+    return domainCounts
+    
 
 def main():
     question2('https://www.youtube.com')
